2c357296281feba2d5ef2c5fb64f56d2ad9d0821
devnolife <andi_agung@student.unismuh.ac.id>
2024-01-26 00:22:53 +0800

    okdeh

--- whatsapp.js
+++ whatsapp.js
@@ -56,5 +56,71 @@ client.on('disconnected', (reason) => {
   client.initialize();
 });
 
+const updateStatus = async (id, status) => {
+  try {
+    await prisma.data_mhs.update({
+      where: {
+        id: id
+      },
+      data: {
+        status: status
+      }
+    })
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// 1000 = 1 detik
+(async () => {
+  const Bombers = async () => {
+    try {
+      const data = await prisma.data_mhs.findMany({
+        where: {
+          nama_fakultas: 'AGAMA ISLAM',
+          status: 'belum'
+        },
+        select: {
+          hp_mahasiswa: true,
+        },
+        take: 1000
+      })
+      data.map((item, index) => {
+        setTimeout(() => {
+          const nomor = phoneNumberFormatter(item.hp_mahasiswa);
+          const message = formatUmum();
+          client.sendMessage(nomor, message)
+            .then(() => {
+              updateStatus(item.id, 'sudah');
+            }).catch(() => {
+              updateStatus(item.id, 'gagal');
+            })
+        }, index * 1000);
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  // await Bombers();
+
+  const TestChat = async () => {
+    try {
+      const number = phoneNumberFormatter('085255113333');
+      const message = formatUmum();
+      client.sendMessage(number, message)
+        .then(() => {
+          console.log('success');
+        }).catch(() => {
+          console.log('error');
+        })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  await TestChat();
+
+})();
+
 
 
